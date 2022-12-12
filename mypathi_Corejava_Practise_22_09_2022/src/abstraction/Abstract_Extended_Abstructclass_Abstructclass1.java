c3943b332470e7b4cc3323069fe91c947dad33e4
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 15:28:07 +0530

    dayy1

---
+++ mypathi_Corejava_Practise_22_09_2022/src/abstraction/Abstract_Extended_Abstructclass_Abstructclass1.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public class Abstract_Extended_Abstructclass_Abstructclass1 extends   AbstructClass1  {
+
+public void m1() {
+	
+	System.out.println("m1");
+}
+  public void m7()
+  {
+	  System.out.println("m7");
+  }
+    
+}
