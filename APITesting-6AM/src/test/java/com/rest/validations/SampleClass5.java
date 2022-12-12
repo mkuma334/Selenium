bc27949e024a6953aa35e15f9517cf988eadff93
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 17:21:48 +0530

    dfbv

--- APITesting-6AM/src/test/java/com/rest/validations/SampleClass5.java
+++ APITesting-6AM/src/test/java/com/rest/validations/SampleClass5.java
@@ -5,7 +5,7 @@ public class SampleClass5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("sampleclass5 is executeddd");
+		System.out.println("sampleclass5 is executeddddd");
 
 	}
 
