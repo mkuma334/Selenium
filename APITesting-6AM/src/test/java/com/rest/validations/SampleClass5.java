118f7ca46fd825efc07686b68418102821fb3bc8
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 17:14:01 +0530

    5

---
+++ APITesting-6AM/src/test/java/com/rest/validations/SampleClass5.java
@@ -0,0 +1,12 @@
+package com.rest.validations;
+
+public class SampleClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("sampleclass5 is executed");
+
+	}
+
+}
