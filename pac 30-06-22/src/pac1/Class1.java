7fbf534f1c05447835147b97c920944b42864e3b
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 17:07:00 +0530

    ccc

---
+++ pac 30-06-22/src/pac1/Class1.java
@@ -0,0 +1,18 @@
+package pac1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class1 {
+
+	public void m1()
+	{
+		WebDriver driver=new ChromeDriver();
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("hi");
+	}
+
+}
