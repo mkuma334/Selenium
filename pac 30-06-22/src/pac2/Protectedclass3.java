e656e45471a47fea2be165f266df022924d65853
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 17:19:20 +0530

    ppp

---
+++ pac 30-06-22/src/pac2/Protectedclass3.java
@@ -0,0 +1,14 @@
+package pac2;
+import pac1.Protectedclass1;
+import pac1.Protectedclass1;
+
+ class Protectedclass3 extends Protectedclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Protectedclass3 y=new Protectedclass3();
+     Protectedclass1 v=new Protectedclass1();
+    
+	}
+
+}
