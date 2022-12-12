75b9c5a69b4e7ac7de2e5274b5405c5e2e2260f9
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 15:27:38 +0530

    day1

---
+++ mypathi_Corejava_Practise_22_09_2022/src/abstraction/Abs3.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public  class Abs3 extends Abs1 {
+  public void m1()
+  {
+	  System.out.println("m1");
+  }
+  
+  public static void main(String[] args) {
+	  Abs3 x=new Abs3();
+	  x.m1();
+  }
+	
+}
