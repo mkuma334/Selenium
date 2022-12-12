8d0cd4dd9bd0af84f9ebf08900723b6645b50da0
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 15:47:27 +0530

    day1 Selenium

--- APITesting-6AM/src/test/java/sampleclass2.java
+++ APITesting-6AM/src/test/java/sampleclass2.java
@@ -3,7 +3,7 @@ public class sampleclass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println(" Hi sampleclass2");
+    System.out.println(" Hi sampleclass222");
 	}
 
 }
