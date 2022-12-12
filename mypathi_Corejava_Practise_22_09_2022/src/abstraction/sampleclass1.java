133bb1460458b7114b51e3a59f60b05f4b4a290f
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 15:35:42 +0530

    ccc

---
+++ mypathi_Corejava_Practise_22_09_2022/src/abstraction/sampleclass1.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public class sampleclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hi this is manoj");
+	}
+
+}
