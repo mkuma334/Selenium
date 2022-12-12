6badae17d3450ced1a9e3143b6670da6183d107d
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 15:25:02 +0530

    day1

---
+++ APITesting-6AM/src/test/java/com/test/java/EncapsulationMain.java
@@ -0,0 +1,19 @@
+package com.test.java;
+
+public class EncapsulationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Encapsulatuon x=new Encapsulatuon();
+		
+		
+		//x.setMassage();
+		x.getMessage("Manoj");
+		
+		//String x1=x.setMassage();
+		
+		System.out.println("x1");
+	}
+
+}
