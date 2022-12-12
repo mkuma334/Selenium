30579dc7fc2315caacd32183c2dd44d69166242d
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 15:27:11 +0530

    fg

---
+++ mypathi_Corejava_Practise_22_09_2022/src/loops/WhileLoop.java
@@ -0,0 +1,34 @@
+package loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int count=0;
+         String name="manoj";
+      //   abc:
+        // while(true)
+        // {
+        	 
+        	switch (name) {
+			case "manojj": 
+				System.out.println("name is not matchh");
+				
+				break;
+			case "kumar": System.out.println("name is not match");
+			break;
+			default:
+				System.out.println("no one match");
+			//	break abc ;
+				
+				System.out.println("default continue");
+			}
+			
+			/*
+			 * count++; if(count==3) { break; }
+			 */
+			 
+         //}
+	}
+
+}
