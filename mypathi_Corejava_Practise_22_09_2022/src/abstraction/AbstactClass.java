5e8873e7859ab47315f9d31ffcb399819e6e7b91
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 15:28:57 +0530

    vbvvv

---
+++ mypathi_Corejava_Practise_22_09_2022/src/abstraction/AbstactClass.java
@@ -0,0 +1,16 @@
+package abstraction;
+
+public abstract class AbstactClass 
+{
+	public abstract void m1();
+	
+	public void m2()
+	{
+		System.out.println("m1 method in abstact class is executed");
+	}
+	
+
+		
+		
+
+}
