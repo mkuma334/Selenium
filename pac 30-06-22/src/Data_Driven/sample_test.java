095fe7663b382a862d8d762c3c106d8832eab6a6
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 17:26:56 +0530

    sampleclass6

---
+++ pac 30-06-22/src/Data_Driven/sample_test.java
@@ -0,0 +1,27 @@
+package Data_Driven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample_test {
+  @Test
+  public String  m1 () 
+  {
+	String a="manoj";
+	 
+	return a;
+	 
+  }
+  
+  @Test
+  public int s1 ()
+  {
+	  int b=10;
+	//  Reporter.log();
+	 return b;
+	  
+  }
+  
+  
+  
+}
