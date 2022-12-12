7c95f25d17cb7d18b1552c1926a775d1499417da
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 17:06:39 +0530

    sampple4

--- pac 30-06-22/src/SampleClass4.java
+++ pac 30-06-22/src/SampleClass4.java
@@ -4,7 +4,7 @@ public class SampleClass4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   
-		System.out.println("sampleclass 4 is executeddd");
+		System.out.println("sampleclass 4 is executed");
 	}
 
 }
