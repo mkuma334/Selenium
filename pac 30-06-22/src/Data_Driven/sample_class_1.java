13d6e58345919a10e2f78f9198389164b647557d
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-12 17:24:24 +0530

    Sampleclass6

---
+++ pac 30-06-22/src/Data_Driven/sample_class_1.java
@@ -0,0 +1,24 @@
+package Data_Driven;
+
+public class sample_class_1 
+{
+	public String m1()
+	{
+		return "manoj";
+	}
+	
+	public int s1() {
+		return 10;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sample_class_1 a=new sample_class_1();
+		System.out.println(a.m1());
+		
+		System.out.println(a.s1());
+
+	}
+
+}
