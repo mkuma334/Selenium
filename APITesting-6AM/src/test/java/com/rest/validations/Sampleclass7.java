6212362aacc3982aae801b445583a2c89f642305
Manoj <Manoj@LAPTOP-F1PFHLE7>
2022-12-14 15:12:50 +0530

    SampleClass7

---
+++ APITesting-6AM/src/test/java/com/rest/validations/Sampleclass7.java
@@ -0,0 +1,12 @@
+package com.rest.validations;
+
+public class Sampleclass7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hi SampleClass7   ");
+	}
+
+}
